18d8a3593c3dae6a7ac58da713da1a1f02bc48df
Eric Hasselbring <ehasselbring@gmail.com>
2016-09-15 15:53:37 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)  # I don't think a method is necessary.. 
+  # board.count{|token| token == "X" || token == "O"}
+
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
+    end
+  end
+  turns
+
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
