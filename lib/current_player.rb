c27be03f4581201a952c2b3b025cb200d7780177
Rory Aptekar <me@roryaptekar.com>
2016-12-04 06:56:55 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,10 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
+  turn_counter(board) % 2 == 0 ? "X" : "O"
+/*  if turn_count(board) % 2 == 0
     return "X"
   else
     return "O"
-  end
+  end*/
 end
