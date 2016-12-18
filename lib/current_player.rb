dea6650ec809a46629b3074ec0e83563f281680f
Duncan Maina <duncandevs@gmail.com>
2016-12-18 00:53:39 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+  turns
+end
+
+def current_player(board)
+  if turn_count(board)  % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+
+
+
