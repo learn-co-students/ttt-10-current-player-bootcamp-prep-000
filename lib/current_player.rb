ccd243fa62fcfdac6b03322899647aa8a33ea4ca
Adrian Davis <chuck.d.kc@gmail.com>
2017-01-04 22:45:34 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,9 +10,5 @@ end
 
 
 def current_player(board)
-    if turn_count(board) % 2 == 0
-        return 'X'
-    else
-        return 'O'
-    end    
+    turn_count(board) % 2 == 0 ? 'X' : 'O'
 end
