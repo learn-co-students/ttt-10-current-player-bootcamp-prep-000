e1b20bc34ab7cbf855bc510149437aadac3119bc
Egor Lebedev <mattspring0@gmail.com>
2017-01-04 07:44:29 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count (board)
+    count = 0
+    board.each do |turn|
+        if turn != " "
+            count += 1
+        end
+    end
+    count
+end
+
+def current_player (board)
+    count = turn_count(board)
+    if count % 2 == 0
+        return 'X'
+    end
+    return "O"
+end
