92fb6c40c818c2d458f849655bad75d3488ade91
Adrian Davis <chuck.d.kc@gmail.com>
2017-01-04 22:40:22 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+    turns=0
+    board.each do |boards|
+        if boards=='X' || boards =='O'
+            turns+= 1
+        end        
+    end    
+    return turns
+end
+
+
+def current_player(board)
+    if turn_count(board) % 2 == 0
+        return 'X'
+    else
+        return 'O'
+    end    
+end
