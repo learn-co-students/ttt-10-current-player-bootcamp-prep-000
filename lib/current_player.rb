62a559ee2f38910f8d521ab444ab6d08b8dbd462
Vagelis Kalimeris <aka9080@yahoo.gr>
2017-02-21 18:34:52 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  count = 0
+  board.each do |spot|
+    if spot == "X" || spot == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  times = turn_count(board)
+  if times % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
