4c61754174c033f4d8f06cafc8196970378a6bc1
Spencer Boakye <spencerboakye@gmail.com>
2017-03-15 13:46:48 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+
+def turn_count(board)
+  counter = 0
+  board.each {|element|
+    if element != " " && element != ""
+      counter += 1
+    end}
+    counter
+  end
+
+def current_player (board)
+turn_count(board) % 2 == 0 ? "X" : "O"
+end
