e095540bf5f9f4d091bed48387a991b58b2c8d1a
Tracy Copeland <ayerik@hotmail.com>
2017-03-18 21:25:01 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count (board)
+  count = 0
+  
+  board.each do |space|
+    if !(space == " ")
+      count += 1
+    end
+  end
+
+  count
+end
+
+def current_player (board)
+  count = turn_count(board)
+  if (count % 2 == 0)
+    return "X"
+  else
+    return "O"
+  end
+end
