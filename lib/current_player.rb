0fa02b0b702a682a5638e428b06f20bae2ee832c
Richard Steinbach <steinbach.dick@gmail.com>
2017-04-26 05:43:56 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+
+def current_player(board)
+turn = turn_count(board)
+  if turn % 2 == 0
+    return "X"
+  else return "O"
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |place|
+    if place == "X" || place == "O"
+      count += 1
+    end
+  end
+  return count
+end
