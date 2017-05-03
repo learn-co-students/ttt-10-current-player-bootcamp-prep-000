d6ec49cb17f5b469fb9e2221eda9bd9da8dcfb51
Matt Thorry <mthorry@gmail.com>
2017-05-03 21:23:39 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,22 +8,27 @@ def turn_count(board)
   current_turn
 end
 
+# MY ORIGINAL
+# def current_player(board)
+#   current_turn = 0
+#   player = ""
+#
+#   board.each do |space|
+#     if space == "X" || space == "O"
+#       current_turn += 1
+#     end
+#   end
+#
+#   if current_turn.even?
+#     player += "X"
+#   else
+#     player += "O"
+#   end
+#
+#  player
+# end
 
+# MUCH BETTER!
 def current_player(board)
-  current_turn = 0
-  player = ""
-
-  board.each do |space|
-    if space == "X" || space == "O"
-      current_turn += 1
-    end
-  end
-
-  if current_turn.even?
-    player += "X"
-  else
-    player += "O"
-  end
-  
- player
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
