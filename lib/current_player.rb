73abda2b4782e104bc323d93fc7fcd7ed9b69ef5
Mikka Pineda <mikkanthrope@gmail.com>
2017-05-05 21:12:10 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |cell|
-    if cell != nil && "#{cell}" == "X" || "#{cell}" == "O"
+    if cell != nil && cell == "X" || cell == "O"
        counter += 1
     end
   end
