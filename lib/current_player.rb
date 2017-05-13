955b54749a65714962c8c60e2a18364dc5fed239
Md Abrar Akash <akash8663@gmail.com>
2017-05-13 02:46:24 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,25 @@
+#board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+def turn_count board
+  counter = 0
+  board.each do |value|
+    if value == "X" || value == "O"
+      counter += 1
+    end
+  end
+  return counter
+  #current_player board
+end
+#turn_count board
+def current_player board
+  counter = 0
+  board.each do |value|
+    if value == "X" || value == "O"
+      counter += 1
+    end
+  end
+  if counter.even?
+    return "X"
+  else
+    return "O"
+  end
+end
