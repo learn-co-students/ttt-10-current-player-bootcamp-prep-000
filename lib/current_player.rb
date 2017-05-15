1db7142b94b736075c2ac62c53dd68a9239be960
Joseph Kim <josephkim0224@gmail.com>
2017-05-15 15:39:14 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,10 +5,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn = turn_count(board)
-  if turn % 2 == 0
-    'X'
-  else
-    'O'
-  end
+  turn_count(board) % 2 == 0 ? 'X' : 'O'
 end
