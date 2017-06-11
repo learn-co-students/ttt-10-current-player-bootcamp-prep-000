be4de35a17813662feec724686916fa40e8003a7
Andrew James <ajames20@gmail.com>
2017-06-11 16:00:11 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+  count = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      count += 1
+    end
+  end
+  return count #interger
+end
+
+def current_player(board)
+  (turn_count(board).even?) ?  "X" :   "O"
+end
