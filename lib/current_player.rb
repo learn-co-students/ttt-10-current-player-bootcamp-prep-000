efe73d69edd5baaf419defa870f1244c5819c82f
Oliver Curting <oc@hejverden.dk>
2017-07-06 02:45:06 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+
+  board.each do |index|
+    if index != " "
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
