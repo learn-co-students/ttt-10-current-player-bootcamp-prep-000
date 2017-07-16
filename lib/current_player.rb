0efa74e586388b3041d1fff0f71e1321d9cc79a4
Christina Pan <christina.pl.pan@gmail.com>
2017-07-16 12:57:24 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(array)
+  count = 0
+  array.each do |position|
+    if position == "X" || position == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
