c187456be09f91deb510f7a090faf56990a2f37c
Nicky Liu <nickyliu91@gmail.com>
2017-07-20 09:45:31 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,6 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even?
-    return "X"
-  elsif turn_count(board).odd?
-    return "O"
+  symbol = turn_count(board).even? ? "X": "O"
   end
 end
