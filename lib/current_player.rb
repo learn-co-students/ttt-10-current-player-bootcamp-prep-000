0e8c192c234316523ac0e6335174c26298624a20
Eric Lau <cireual510@gmail.com>
2017-08-03 21:46:19 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+def turn_count(board)
+  count = 0
+  board.each do |input|
+    if input != " "
+    count += 1
+  end
+end
+  return count
+end
+
+=begin
+def current_player(board)
+  if turn_count(board)%2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+=end
+def current_player(board)
+  turn_count(board)%2 ==0 ? "X" : "O"
+end
