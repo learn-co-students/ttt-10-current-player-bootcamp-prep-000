95d72b1d4ff6911c8cb4167a30e524222287c31b
Mohammad Khan <sparrowangels@gmail.com>
2017-08-06 00:30:21 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,11 +11,14 @@ end
 def current_player(board)
   #use the #turn_count method to determine if it is "X"'s turn or "O"'s.
   #If the turn count is an even number, the #current_player method should return "X", otherwise, it should return "O".
-  if (turn_count(board) % 2 == 0)
-    return "X"
-  else
-    return "O"
-  end
+  #works
+  # if (turn_count(board) % 2 == 0)
+  #   return "X"
+  # else
+  #   return "O"
+  # end
+  value = turn_count(board) % 2 == 0 ? "X" : "O"
+
 end
 
 
