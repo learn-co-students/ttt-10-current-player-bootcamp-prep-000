d27b1ddf7806b0f50e2b6daff3b175ca6ba647b6
cunhasb <cunhasb@aol.com>
2017-08-23 00:55:25 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+  counter = 0
+  board.each do |cell|
+    if cell == "X" || cell == "O"
+      counter += 1
+    end
+end
+counter
+end
+
+
+def current_player(board)
+
+  turn_count(board) % 2 == 1 ? "O" : "X"
+end
