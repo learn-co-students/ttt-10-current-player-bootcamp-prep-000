66354d90421d7e01015020e336f8f3ddfdf3a668
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-08-29 22:08:35 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+
+def turn_count(board)
+  counter = 0
+  board.each do |item|
+       if "#{item}" != " "
+        counter += 1
+      end
+  end
+return counter
+end
+
+
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
