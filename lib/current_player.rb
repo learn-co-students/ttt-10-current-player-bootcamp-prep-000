a8cfa367951b5f804c089ca3df812c8399ee51b7
Issam Issam <shninder5454@gmail.com>
2017-08-30 19:31:13 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  count = 0
+  board.each do |item|
+    if item=="X" || item=="O"
+      count +=1
+    end
+  end
+  return count
+end
+def current_player(board)
+  return turn_count(board).even? ? "X" : "O"
+end
