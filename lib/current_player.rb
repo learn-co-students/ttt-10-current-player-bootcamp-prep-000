64fef261770eb895a663a1c7f2913b9359120531
W M <wmendoz7@calstatela.edu>
2017-09-08 04:02:09 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  turns = 0
+  board.each do |element|
+    if(element == "X" || element == "O" )
+      turns+= 1
+    end
+  end
+  return turns
+end
+
+
+def current_player(board)
+  (turn_count(board).even?) ? "X": "O"
+end
