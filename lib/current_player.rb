de4ad55322d4a88feba973fa51c3a065fa55db58
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-20 20:29:28 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,19 @@
 def turn_count(board)
-  turn = 1
+  turn = 0
   board.each do |space|
-    if turn != " "
+    if space != " "
       turn += 1
     end
   end
   turn
+end
+
+def current_player(board)
+  turns_played = turn_count(board)
+  if turns_played%2 == 0
+    current_player = "X"
+  else
+    current_player = "O"
+  end
+  current_player
 end
