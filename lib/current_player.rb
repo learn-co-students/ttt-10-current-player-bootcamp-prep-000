7fc340d5977fb21fd6ee393cbe96fbb4a8207bb4
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-29 18:50:29 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+#define turn_count
+
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O";
+      counter += 1;
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0;
+    return "X"
+  else
+    return "O"
+  end
+end
