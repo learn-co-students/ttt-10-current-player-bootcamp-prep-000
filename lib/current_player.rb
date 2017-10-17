721122fe51a97ca628c782ed4be91e3039a022c7
Simcha Wood <truthfulthomas@gmail.com>
2017-10-17 00:05:54 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  counter = 0
+
+  board.each do |square|
+    if square != " "
+      counter += 1
+    end
+  end
+
+  return counter
+end
+
+def current_player(board)
+  turn = turn_count(board)
+  if (turn % 2 == 0)
+    return "X"
+  else
+    return "O"
+  end
+end
