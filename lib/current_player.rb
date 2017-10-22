6b91cb1b2c8e57a884c3ea242b3fd69f8639d52e
Jacob Miller <jake0miller@gmail.com>
2017-10-22 11:19:48 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(board)
+  count = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  return turn_count(board).even? ? "X" : "O"
+end
