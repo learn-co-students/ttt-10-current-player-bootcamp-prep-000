c06e91c583b8bdb859853a3cbc5fe62faa29e073
darraghmcalone@hotmail.co.uk <darraghmcalone@hotmail.co.uk>
2017-11-22 20:09:43 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  if count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
