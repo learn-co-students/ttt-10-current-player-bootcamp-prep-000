3608c62db7fa196f981e9aaee8563b0ec8718a26
Victoria <vitalina27@gmail.com>
2017-12-11 15:07:07 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  counter = 0
+  board.each do |loc|
+    if loc != " "
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) %2 == 0
+    "X"
+  else "O"
+  end
+end
