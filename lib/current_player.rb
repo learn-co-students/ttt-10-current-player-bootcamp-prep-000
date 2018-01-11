e5cd0917c0830a20e9342ce6199932ca5da31759
Jonathan Valdesuso <jbvaldesuso@gmail.com>
2018-01-11 07:37:31 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,16 +2,12 @@ def turn_count(board)
   count = 0 
   board.each do |position|
     if position != " " && position != "" && position != nil 
-      count += 1 
+      count += 1
     end
   end
   count
 end
 
 def current_player(board)
-  if turn_count(board).even?
-    return "X"
-  else
-    return "O"
-  end
+  turn_count(board).even? ? "X" : "O"
 end
