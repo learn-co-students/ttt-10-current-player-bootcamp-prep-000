ba9bf0c24efb2c2d5657c4bbd5628103669c2d78
Sean Derrow <sderrow@mit.edu>
2018-01-23 15:39:08 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,16 +1,7 @@
 def turn_count(board)
-  count = 0
-  board.each do |x|
-    if x != " "
-      count += 1
-    end
-  end
-  count
+  (board.collect { |x| x == " " ? 0 : 1 }).inject(:+)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  end
-  "O"
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
