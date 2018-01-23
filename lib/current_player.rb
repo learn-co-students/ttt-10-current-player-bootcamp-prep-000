b65cd144e12d43cfb7990d96ee50723c4e050daf
Sean Derrow <sderrow@mit.edu>
2018-01-23 05:37:00 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  count = 0
+  board.each do |x|
+    if x != " "
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  end
+  "O"
+end
