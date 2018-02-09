e91f98b4a57c684b49a1a58c1721beb602000106
Micah Shute <micah.shute@gmail.com>
2018-02-09 00:06:00 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  turn = 0
+  board.each do |space|
+    if space.strip == "X" || space.strip =="O"
+      turn += 1
+    end
+  end
+  return turn
+end
+  
+def current_player(board)
+  return turn_count(board).even? ? "X" : "O"
+end
+  
