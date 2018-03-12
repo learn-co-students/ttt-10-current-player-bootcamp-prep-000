d3884224f051419dc86595d25030c0a8e48e491e
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-12 23:07:14 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+
+def turn_count(board)
+  i = 0
+  board.each do |move|
+    move == "X" || move == "O" ? i += 1 : i += 0
+  end
+  i
+end
+
+def current_player(board)
+  curr = turn_count(board) % 2
+  curr == 0 ? "X" : "O"
+end
