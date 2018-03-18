5e40165d5fb5acdd793cbf347dd1ee3b2410d776
Shirley Collins <shirley.ednave@gmail.com>
2018-03-18 00:07:07 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  turns = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      turns += 1
+    end
+  end
+  return turns
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  current_player = (turns.to_i % 2) == 0 ? "X" : "O"
+end
