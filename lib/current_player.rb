694aab67ef2cead4287cbffcf8d806644922a7b7
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-03-27 07:03:52 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,20 @@
+# def turn_count(board)
+#   turns = 0
+#   x_count = 0
+#   o_count = 0
+#   board.each do | spot |
+#     x_count+= 1 if spot == "X"
+#     o_count+= 1 if spot == "O"  
+#   end
+#   turns = x_count + o_count
+#   turns
+# end
+
 def turn_count(board)
   turns = 0
-  x_count = 0
-  o_count = 0
   board.each do | spot |
-    x_count+= 1 if spot == "X"
-    o_count+= 1 if spot == "O"  
+    turns+= 1 if spot == "X" || spot == "O"
   end
-  turns = x_count + o_count
   turns
 end
 
