28a62bbb90bd9ddddd2729002c7fb93984b486cf
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-04-11 09:30:43 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  count = 0 
+  board.each do |field|
+    if field == "X" || field == "O"
+      count +=1 
+    end
+  end
+  return count
+end
+
+def currnt_player(board)
+  count = turn_count(board)
+  if count %2 == 0 
+    return "X"
+  else
+    return "O"
+  end
+end
+  
+end
