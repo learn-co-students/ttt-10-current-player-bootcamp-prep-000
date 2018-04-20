acc58341193ad0ce7b1a15646bf3a9a7283f5e1c
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-20 15:23:11 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,18 @@
 def turn_count(board)
-  counter = 0
-  x_or_o = ["X", "O"]
-  x_or_o.each do |board|.length
-    counter += 1
+ counter = 0
+
+  board.each do |turn|
+    if turn != " "
+      counter += 1
+    end
   end
+  return counter
+end
+
+def current_player(board)
+ if turn_count(board).even? == true
+  return "X"
+ else
+   return "O"
+ end
 end
