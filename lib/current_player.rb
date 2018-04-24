8136d284c63d1cbdb30221f4cfb6034d510d7fac
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-24 16:23:24 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,54 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+    turns = 0
+   board.each do |token|
+     if token == "X" || token == "O"
+         turns+=1 
+   end
+ end
+ turns
+end
+
+def current_player(board) 
+ if turn_count(board) % 2 == 0 
+    "X"
+  else
+    "O"
+    end
+  end
