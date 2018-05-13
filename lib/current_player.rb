e3dc3ee45d9fee7f1b1e8a594ef64f9d2beb3439
Jess H <jessdev83@gmail.com>
2018-05-13 22:48:52 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+counter = 0 
+board.each do |board|
+    if (board == "X") || (board == "O")
+      counter +=1 
+    end
+  end
+  return counter
+end
+
+
+def current_player(board)
+  if turn_count(board).even? 
+    return "X"
+  else 
+    return "O"
+    end
+  end
+
