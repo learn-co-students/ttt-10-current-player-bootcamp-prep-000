ef16a9a560bf5b2ad0a492a74bfb75b09f404a0e
Loren Cerri <plexidev@gmail.com>
2018-06-23 09:11:45 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,6 +8,8 @@ def turn_count(board)
   counter
 end
 
+# Notes: You can also use the .even or .odd methods after
+# calling #turn_count
 def current_player(board)
   if turn_count(board) % 2 == 0
     "X"
