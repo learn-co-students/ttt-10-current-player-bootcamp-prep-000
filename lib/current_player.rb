e9aa7f5f72810bd4f5fd9264cc4eefc90be1e09c
Loren Cerri <plexidev@gmail.com>
2018-06-23 09:10:50 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,17 @@
 def turn_count(board)
   counter = 0
   board.each do |cell|
-    if (cell != " ") counter += 1
+    if cell != " " 
+      counter += 1
+    end
   end
   counter
 end
 
-def current_player
-  
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
 end
