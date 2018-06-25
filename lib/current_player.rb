dc6656365f33b2f36c899f9f3b45ebde32ca6ea0
rajeshkainth@hotmail.co.uk <rajeshkainth@hotmail.co.uk>
2018-06-25 16:07:31 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+board = Array 
+board = [" "," "," "," "," "," "," "," "," "]
+ 
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+ 
+ def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+ 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
