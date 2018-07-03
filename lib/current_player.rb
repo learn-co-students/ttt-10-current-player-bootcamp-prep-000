6878a3bda7d542a1636385d1261200d68c6c0c41
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-03 00:18:22 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,19 +1,39 @@
 def turn_count(board)
   turn=0  
   board.each do|spot|
+<<<<<<< HEAD
     if (spot.strip=="X" || spot.strip=="O"||spot.strip=="x" || spot.strip=="o")
       turn+=1
     end
   end
+=======
+    if (spot=="X" || spot=="O")
+      turn+=1
+    end
+  end
+  puts "totasl turns is #{turn}"
+>>>>>>> 80d813c0e156fe904fdd08d965f44ed8a1fc3ae2
 return turn
 end
 
 def  current_player(board)
+<<<<<<< HEAD
   turn=turn_count(board)
   puts "totasl turns is #{turn}"
-  if (turn%2==0)
-    return "X"
-  else 
-    return "O"
+  turn%2==0 ? "X":"O"
   end
 end
+=======
+  turns=turn_count(board)
+  if (turns%2)
+    return "O"
+  else 
+    return "X"
+  end
+end
+
+
+
+board = [" ", "O", " ", " ", "", " ", " ", " ", "X"]
+puts current_player(board)
+>>>>>>> 80d813c0e156fe904fdd08d965f44ed8a1fc3ae2
