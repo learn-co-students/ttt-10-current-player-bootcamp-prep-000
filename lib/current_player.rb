b802e5795b86755b3aae1730fe457ab195254b2b
Kvasemi <alancho@gmail.com>
2018-07-05 03:23:26 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+#! /usr/bin/env ruby
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+# def current_player(board)
+#   if turn_count(board) % 2 == 0
+#     "X"
+#   else
+#     "O"
+#   end
+# end
+
+def turn_count(board)
+  count = 0 
+  board.each do |marker|
+    if marker == 'X' || marker == 'O'
+      count += 1
+    end
+  end
+  count
+end
