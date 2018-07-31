ab0b14e07401a95b81d9d44e1ea96516f10a6288
Jin Ai Yap <jinai.jyap@gmail.com>
2018-07-31 11:28:10 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+  turns
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+  # if turn_count(board) % 2 == 0
+  #   "X"
+  # else
+  #   "O"
+  # end
+end
