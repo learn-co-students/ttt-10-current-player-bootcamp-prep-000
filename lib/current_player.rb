0575ae6502133b88a94ae497dbafed358c1c6407
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-03 07:12:23 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  counter = 0
+board.each do |character|
+  if character == "X" || character == "O"
+  counter += 1
+end
+end
+counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    "X"
+  else 
+    "O"
+end
+end
