d84a8252971c95ffb85136b821ba627c529c052c
S H <sashanicolehart@gmail.com>
2018-09-11 00:52:12 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      count += 1 
+    end
+  end
+  return count
+end
