27386193266ea579f01883fd12260c3e48688cca
Annie Eby <eby.annie@gmail.com>
2018-09-23 17:41:46 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -54,10 +54,22 @@ end
 
 def current_player(board)
 turn_count(board) 
-  if turn_count(board) % 2 == 0 
-    return "X"
-  else 
-    return "O"
-  end
+  turn_count(board) % 2 == 0 ? "X" :"O"
 end
+
+
+#not ternary operator method for current_player
+
+#def current_player(board)
+#turn_count(board) 
+#  if turn_count(board) % 2 == 0 
+#    return "X"
+#  else 
+#    return "O"
+#  end
+#end
+
+
+
+
 
