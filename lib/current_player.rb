64131e297073db0f49a72374620afe0ac7ba33b5
github username <github email address>
2018-10-04 20:08:45 -0400

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board) # board is an array
+  counter = 0
+  board.each do |char|
+    if char == "X" || char == "O" # if space occupied
+      counter +=1
+    end
+  end
+  return counter # return the turn number
+end
+
+def current_player(board)
+  turn = turn_count(board)
+  board.each do |char|
+    if turn == 0 || turn % 2 == 0
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
