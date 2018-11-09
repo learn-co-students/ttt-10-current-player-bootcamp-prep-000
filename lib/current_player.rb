42ee76a51b8c0b4ae9718f1b3c3cc83e6617534f
Edwin Lin <edwinlin@gmail.com>
2018-11-09 01:58:23 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,10 +11,10 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
-
+  # if turn_count(board) % 2 == 0
+  #   return "X"
+  # else
+  #   return "O"
+  # end
+ turn_count(board) % 2 == 0 ? "X":"O"
 end
