ba69ad8b8ca33c38a1d4b48af5f176735e7934ee
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-10 19:32:31 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  counter = 0
+  board.each do |moves|
+    if moves == "X" || moves == "O"
+      counter += 1
+    end
+  end
+  return counter
+end 
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else 
+    "O"
+  end
+end
