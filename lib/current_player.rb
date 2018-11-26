b7693a8039522368f367eff1ef44c9cddeaf2f5a
Matthew Campbell <matjocampbell@gmail.com>
2018-11-26 02:17:01 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  count = 0
+  
+  board.each { |spot| spot == "X" || spot == "O" ? count+=1 : count+=0 }
+  
+  return count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  
+  if count==0
+    return "X"
+  elsif count%2==0 
+    return "X"
+  else
+    return "O"
+  end
+  
+end
