a5d246eff039cc62678180dbbd16a7b972671537
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-02-21 22:23:58 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+
+
+def turn_count(board)
+  turn = 0
+  board.each do |ele| 
+    if ele == "X" || ele == "O"
+    turn += 1
+    end
+  end
+  return turn
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
