18b15e386e85263295c3c05b17ce2e1a06628617
Amari Ellington <amari.l.ellington@gmail.com>
2019-03-03 21:57:30 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(turn)
+
+  move_count = []
+  turn.each do |unit|
+    if unit == "X" || unit == "O"
+      move_count.push(1)
+    end
+  end
+  move_count.length.to_i
+end
+
+def current_player(board)
+    num = turn_count(board)
+    if num % 2 == 0
+      return "X"
+    else
+      return "O"
+    end
+end 
