26480daa6f68ea5e10a98d06dab58a15d4902996
global-debugger-9436 <rossmawd@gmail.com>
2019-03-10 12:29:50 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,16 +1,12 @@
-
 def turn_count(board)
-  i=0
-  turns = 0
-  while i < board.length 
-    if (board[i] == "X" || board[i] == "O")
-      turns += 1
-      i += 1
-    else
-      i += 1
-    end
-  end  
+  
+turns = 0
+  board.each {|l| 
+    if l == "X" || l == "O"
+    turns += 1
+    end    
+    }
   turns
 end
 
-puts "the number of turns is #{turn_count(["X","X","","","O","X","X","X","X"])}"
+puts "the number of turns is #{turn_count(["X","O","O","","O","X","X","X","X"])}"
