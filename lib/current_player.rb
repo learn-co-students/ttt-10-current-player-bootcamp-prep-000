b8859dbf22339f2fff16eb1ec70dd1a5b4adf0eb
Tracie Masek <traciemasek@gmail.com>
2019-06-05 10:48:25 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |el| 
-    if board[board.index(el)] == "X" || board[board.index(el)] == "O"
+    if el == "X" || el == "O"
       counter += 1
     end
   end
