ee0f93e7b50dbb0d7b819387357c62d1f9ce21f4
Skye Geraghty <skyetay03@googlemail.com>
2020-01-10 13:21:00 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,11 +9,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  elsif turn_count(board) % 2 == 1
-    return "O"
-  else
-    return nil
-  end
+  return turn_count(board).even? ? "X" : "O"
 end
