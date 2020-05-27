a4fd4c6f9bfb4b1e215682b76955a5f48f4fae43
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-27 14:00:50 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,59 @@
-var board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+  count=0
+  while count < 9
+   turn(board)
+    count += 1
+  end
+end  
+
 
 def turn_count(board)
-  
-  board.each do |count|
-    puts count
+  count = 0
+  board.each do |board_element|
+    if board_element == "X"
+      puts "#{count}"
+    elsif board_element == "O"
+      puts "#{count}"
+    else
+      count += 1
+    end
   end
-
 end 
 
 
@@ -13,13 +61,14 @@ end
 def current_player(board)
   
   turn_count(board)
-    count = gets.chomp
-    count = input.to_i + 1
+  board_element.to_i + 1
   
-      if count % 2 == 1 
+      if  % 2 == 1 
         "X"  
       else 
         "O"
-      
   end
 end  
+
+
+
