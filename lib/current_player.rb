874422706cacd9824d51567047d626d05c9a103c
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-09-17 17:45:52 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def current_player(board)
+  if turn_count(board) % 2 == 0
+  "X"
+else
+  "O"
+end
+end
+
+def turn_count(board)
+  turns = 0
+  board.each do |player|
+    if player == "X" || player == "O"
+      turns += 1
+    end
+  end
+  turns
+end
