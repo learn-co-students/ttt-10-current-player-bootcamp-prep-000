4a1fb7d011ea4a13bb8d8291b08cd4ce7d822e68
Nahomie ND <nducasse3@gmail.com>
2021-01-27 05:33:49 +0000

    Use iteration to loop over an array, control the return value of a method that uses iteration in the method body. Describe and utilize the (%) operator with ternary opertor or if/else to determine users turn even(X)| odd(O). Call a method from inside of another method.

---
+++ lib/current_player.rb
@@ -0,0 +1,25 @@
+
+def turn_count(board)
+ counter = 0
+  board.each do |turn|
+    
+    if turn == "X" || turn == "O"
+      counter += 1
+   
+    end
+  end
+   counter
+end
+
+def current_player(board)
+  
+ (turn_count(board) % 2) != 1 ? "X" : "O"
+  
+    #if (turn_count(board) % 2) != 1
+      #"X"
+    
+     #else
+       #"O"
+  #end
+end
+
